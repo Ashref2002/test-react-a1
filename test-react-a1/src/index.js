e594c526b4720dfe2e3237a2ab6da752f87322f5
Ashref Saïd <achrefthehuman@gmail.com>
2024-09-21 13:36:18 +0100

    Commit #2

--- test-react-a1/src/index.js
+++ test-react-a1/src/index.js
@@ -3,28 +3,23 @@ import ReactDOM from "react-dom/client";
 import 'bootstrap/dist/css/bootstrap.css';
 
 import Navbar from './components/navbar/navbar';
-import Footer from './components/footer/footer';
-import Slider from './components/slider/slider';
-
 import Home from './pages/home';
 import Contact from './pages/contact';
 import Register from './pages/register';
-import NoPage from './pages/nopage';
 
 export default function App()
 {
-  <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<Navbar/>}/>
-        <Route path='/' element={<Footer/>}/>
-        <Route path='/' element={<Slider/>}/>
-        <Route index element={<Home/>}/>
-        <Route path='Contact' element={<Contact/>}/>
-        <Route path='Register' element={<Register/>}/>
-        <Route path='*' element={<NoPage/>}/>
-        <Route/>
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Navbar />}/>
+          <Route index element={<Home />}/>
+          <Route path='Contact' element={<Contact />}/>
+          <Route path='Register' element={<Register />}/>
+          <Route/>
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
